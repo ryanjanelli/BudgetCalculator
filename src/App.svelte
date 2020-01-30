bb18e398d71bfc006c27b3629e33478de3a2cf1d
ryanjanelli <ryanjanelli@gmail.com>
2020-01-30 08:03:47 -0500

    set up Expense and expensesList

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
+  // components
   import Navbar from "./Navbar.svelte";
+  import ExpensesList from "./ExpensesList.svelte";
+  // data
   import expensesData from "./expenses";
+  // variables
   let expenses = [...expensesData];
 </script>
 
 <Navbar />
-
-
+<main class="content">
+  <ExpensesList {expenses}/>
+</main>
---
+++ src/Expense.svelte
@@ -0,0 +1,1 @@
+<h1>This is a single expense</h1>
