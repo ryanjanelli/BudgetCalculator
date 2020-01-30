af770782fbf61c37652af9b9d22291dad106a02d
ryanjanelli <ryanjanelli@gmail.com>
2020-01-30 15:44:25 -0500

    set up expense buttons with properties and icons

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -1,10 +1,26 @@
 <script>
   export let id;
-  export let name = '';
+  export let name = "";
   export let amount = 0;
 </script>
 
-<div>
-    <h2>title : {name}</h2>
-    <h2>amount : {amount} </h2>
-</div>
+<article class="single-expense">
+
+  <div class="expense-info">
+    <h2>
+      {name}
+      <button class="amount-btn">
+        <i class="fas fa-caret-down" />
+      </button>
+    </h2>
+    <h4>amount : $ {amount}</h4>
+  </div>
+  <div class="expense-buttons">
+    <button class="expense-btn edit-btn">
+      <i class="fas fa-pen" />
+    </button>
+    <button class="expense-btn delete-btn">
+      <i class="fas fa-trash" />
+    </button>
+  </div>
+</article>
