754027998268375829005695ea89a04a99e4176b
ryanjanelli <ryanjanelli@gmail.com>
2020-02-03 13:20:40 -0500

    set name and amount value with two way binding

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -1,5 +1,8 @@
 <script>
   import Title from "./Title.svelte";
+  let name = '';
+  let amount = null;
+//   $: console.log({name, amount})
 </script>
 
 <section class="form">
@@ -7,11 +10,11 @@
   <form class="expense-form">
     <div class="form-control">
       <label for="name">name</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" bind:value={name} />
     </div>
     <div class="form-control">
       <label for="amount">amount</label>
-      <input type="number" id="amount" />
+      <input type="number" id="amount" bind:value={amount} />
     </div>
     <p class="form-empty">please fill out all form fields</p>
     <button type="submit" class="btn btn-block">add expense</button>
