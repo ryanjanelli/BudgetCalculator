8a72df69db05df63a1344374354aa3d034b3ed97
ryanjanelli <ryanjanelli@gmail.com>
2020-02-04 14:34:05 -0500

    set up stagger transition on expense items

--- src/ExpensesList.svelte
+++ src/ExpensesList.svelte
@@ -1,5 +1,6 @@
 <script>
   import { getContext } from "svelte";
+  import { fly } from "svelte/transition";
   // components
   import SectionTitle from "./Title.svelte";
   import Expense from "./Expense.svelte";
@@ -18,7 +19,9 @@
   <SectionTitle title="expense list" />
   <ul>
     {#each expenses as expense, index}
+    <div transition:fly={{x:200, delay:index*700}}>
       <Expense {...expense} />
+    </div>
     {:else}
       <h2>no expenses to list</h2>
     {/each}
