895dc2e68d6b12aa91e2fcc20a7847ef1715c57a
ryanjanelli <ryanjanelli@gmail.com>
2020-02-04 14:25:35 -0500

    set modal window to transition in and out

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,9 +1,9 @@
 <script>
-
+  import { blur, fade, fly } from "svelte/transition";
 </script>
 
-<div class="modal-container">
-  <div class="modal-content">
+<div class="modal-container" in:blur out:fade>
+  <div class="modal-content" in:fly={{y:200, delay:200}} out:fly={{y:-100}}>
     <slot>
       <!-- optional -->
     </slot>
